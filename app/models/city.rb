efc8f32fdaa0f368e861e21c34bd3fb616e9739a
William Cao <williamacao@gmail.com>
2021-06-01 21:34:12 -0700

    commit tree 63d575f7d64e0796c553e41b3eebf2b61595b651

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,31 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+  end
+
+  def self.highest_ratio_res_to_listings
+    cities = self.all
+    highest_city_num = 0
+    highest_city_id = 0
+
+    cities.each do |c|
+      listing_count = c.listings.count
+      reservation_count = 0
+      c.listings.each do |listing|
+        reservation_count += listing.reservations.count
+      end
+
+      ratio = reservation_count.to_f / listing_count.to_f
+
+      if ratio > highest_city_num
+        highest_city_id = c.id
+      end
+    end
+    self.find(highest_city_id)
+  end
+
+  def most_res
+  end
 end
 
